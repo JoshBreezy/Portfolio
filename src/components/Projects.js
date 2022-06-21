db3994744704a1cf07e0a9fc260dcad166fa4d8d
JoshBreezy <81214015+JoshBreezy@users.noreply.github.com>
2022-06-21 11:15:51 -0700

    oh yeah. It's all coming together

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Label } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardGoup, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label } from 'reactstrap';
 import { PROJECTS } from './shared/projects';
 import { baseURL } from './shared/baseURL';
+import { Link } from 'react-router-dom';
+
+
+function RenderProjects () {
+    return (
+        <div className='col col-md-4'>
+            {PROJECTS.map(({id, name, image, link, repo, description}) => (
+
+                    <Card key={id}>
+                        <CardTitle tag='h3'>{name}</CardTitle>
+                        <CardImg top src={baseURL + image} alt={name} />
+                        <CardBody>
+                            <CardText>{description}</CardText>
+                            <CardText>Check out the page <a href={link}><Button color='primary'>here!</Button></a></CardText>
+                            <CardText>Or checkout the github repo <a href={repo}><Button color='primary'>here!</Button></a></CardText>
+                        </CardBody>
+                    </Card>
+
+            ))}
+        </div>
+    )
+}
+
+
 
 
 function Projects() {
     return (
-        <>
-            {PROJECTS.map(({id, name, image, description}) => (
-                <Card key={id}>
-                    <CardImg top src={baseURL + image} alt={name} />
-                    <CardBody>
-                        <CardText>{description}</CardText>
-                    </CardBody>
-                </Card>
-            ))}
-        </>
+        <div className='container'>
+            <div className='row'>
+                <RenderProjects />
+            </div>
+        </div>
     )
 }
 
--- src/components/shared/projects.js
+++ src/components/shared/projects.js
@@ -2,8 +2,9 @@ export const PROJECTS = [
     {
         id: 0,
         name: 'Band Website',
-        image: 'resume/src/components/shared/images/band.png',
+        image: '/images/band.png',
         link: 'https://joshbreezy.github.io/Hurricane-website/',
-        description: 'This is the original webpage I built for my band using only HTML and CSS'
+        repo: 'https://github.com/JoshBreezy/Hurricane-website',
+        description: 'This is the original webpage I built for my band using only HTML and CSS.'
     }
 ]
